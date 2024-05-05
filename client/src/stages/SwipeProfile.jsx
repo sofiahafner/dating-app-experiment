583a048b36138d5e12b6dc3d752155f5473a1071
Franziska Hafner <franziskahafner@Franziskas-MacBook-Pro.local>
2024-05-05 14:15:48 +0100

    added profile data as json

--- client/src/stages/SwipeProfile.jsx
+++ client/src/stages/SwipeProfile.jsx
@@ -6,24 +6,21 @@ import { Player, usePlayer } from '@empirica/core/player/classic/react';
 // }
 
 export function SwipeProfile() {
-    console.log("in SwipeProfile")
     const player = usePlayer()
 
     const handleAccept = () => {
-      console.log('Profile Accepted');
       player.round.set("decision", "accepted")
       player.stage.set("submit", true)
     };
   
     const handleReject = () => {
-      console.log('Profile Rejected');
       player.round.set("decision", "rejected")
       player.stage.set("submit", true)
     };
   
     return (
       <div className="profile-container" style={{ textAlign: 'center', padding: '20px' }}>
-        <img src="https://upload.wikimedia.org/wikipedia/en/0/06/Promising_Young_Woman_poster.jpg" alt="Profile Pic" style={{ width: '200px', height: '200px' }} />
+        <img src="https://raw.githubusercontent.com/sofiahafner/dating-app-experiment/main/profiles/pictures/1.webp" alt="Profile Pic" style={{ width: '200px', height: '200px' }} />
         <div className="profile-info" style={{ margin: '20px' }}>
           <p><strong>Name:</strong> Sofia Hafner </p>
           <p><strong>Age:</strong> 23</p>
