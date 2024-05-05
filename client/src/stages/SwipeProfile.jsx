05c1d40d4baa95109b26b2adcf2b0cb3bb382a4a
Franziska Hafner <franziskahafner@Franziskas-MacBook-Pro.local>
2024-05-05 18:18:15 +0100

    added different profiles for different players

--- client/src/stages/SwipeProfile.jsx
+++ client/src/stages/SwipeProfile.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
 import { Player, usePlayer } from '@empirica/core/player/classic/react';
+import profiles from '../../profiles.json';
 
-// export function SwipeProfile(){
-//     return <div>"SwipeProfile" not yet implemented</div>
-// }
+function findProfileById(profileId) {
+    const profile = profiles.find(p => parseInt(p.profile_ID) === parseInt(profileId));
+    return profile;
+  }
+  
+  function getName(profileId) {
+    const profile = findProfileById(profileId);
+    return profile ? profile.name : "Profile not found";
+  }
+  
+  function getAge(profileId) {
+    const profile = findProfileById(profileId);
+    return profile ? profile.age : "Profile not found";
+  }
+  
+  function getHobby1(profileId) {
+    const profile = findProfileById(profileId);
+    return profile ? profile.hobby1 : "Profile not found";
+  }
+  
+  function getHobby2(profileId) {
+    const profile = findProfileById(profileId);
+    return profile ? profile.hobby2 : "Profile not found";
+  }
+
 
 export function SwipeProfile() {
     const player = usePlayer()
@@ -17,14 +40,26 @@ export function SwipeProfile() {
       player.round.set("decision", "rejected")
       player.stage.set("submit", true)
     };
+
+    const own_profile_id = player.round.get("ownProfileID")
+    console.log(own_profile_id)
+
+    const own_profile_name = getName(own_profile_id)
+    const own_profile_age = getAge(own_profile_id)
+    const own_profile_hobby1 = getHobby1(own_profile_id)
+    const own_profile_hobby2 = getHobby2(own_profile_id)
+
+    // const profile = findProfileById(1);
+    // console.log(profile);
+    const link_to_profile_picture = "https://raw.githubusercontent.com/sofiahafner/dating-app-experiment/main/profiles/pictures/"+ own_profile_id +".webp"
   
     return (
       <div className="profile-container" style={{ textAlign: 'center', padding: '20px' }}>
-        <img src="https://raw.githubusercontent.com/sofiahafner/dating-app-experiment/main/profiles/pictures/1.webp" alt="Profile Pic" style={{ width: '200px', height: '200px' }} />
+        <img src={link_to_profile_picture} alt="Profile Pic" style={{ width: '200px', height: '200px' }} />
         <div className="profile-info" style={{ margin: '20px' }}>
-          <p><strong>Name:</strong> Sofia Hafner </p>
-          <p><strong>Age:</strong> 23</p>
-          <p><strong>Hobbies:</strong> Doing my Thesis</p>
+          <p><strong>Name:</strong> {own_profile_name} </p>
+          <p><strong>Age:</strong> {own_profile_age} </p>
+          <p><strong>Hobbies:</strong>{own_profile_hobby1}, {own_profile_hobby2}</p>
         </div>
         <div>
           <button onClick={handleAccept} style={{ backgroundColor: 'green', color: 'white', padding: '10px 20px', marginRight: '10px', border: 'none', borderRadius: '5px' }}>
--- server/src/callbacks.js
+++ server/src/callbacks.js
@@ -5,11 +5,10 @@ Empirica.onGameStart(({ game }) => {
   const treatment = game.get("treatment");
   const { numSwipes } = treatment; // Destructuring numRounds from treatment
 
-  console.log("in onGameStart");
-  console.log(treatment); // Using destructured numRounds
-
-  // const numRounds = 10
-
+  // for (const player of players){
+  //   const ownProfileID = 1;
+  //   player.round.set("ownProfileID", ownProfileID)
+  // }
   for (let index = 0; index < numSwipes; index++) {
     const round = game.addRound({
       name: `Round ${index + 1}`,
@@ -19,13 +18,37 @@ Empirica.onGameStart(({ game }) => {
     round.addStage({name: "swipeProfile", duration: 10});
     round.addStage({name: "Results", duration: 1});
   }
+  
 });
 
 
 
-Empirica.onRoundStart(({ round }) => {});
 
-Empirica.onStageStart(({ stage }) => {});
+
+Empirica.onRoundStart(({ round }) => {
+  const players = round.currentGame.players
+  let profile_id = 1
+  for (const player of players){
+    player.round.set('ownProfileID', profile_id)
+    profile_id = profile_id + 1
+  }
+  // const players = stage.currentGame.players
+  // const allProfileIDs = [1,2,3,4]
+  
+  // for (let index = 0; index < players.length; index++) {
+  //   const player = players[index]
+  //   const ownProfileID = allProfileIDs[index]
+  //   player.round.set("ownProfileID", ownProfileID)
+  //   console.log("callbacks ownProfileID")
+  //   console.log(ownProfileID)
+  // }
+  console.log("in onRoundStart")
+});
+
+Empirica.onStageStart(({ stage }) => {
+  console.log("in onStageStart")
+
+});
 
 Empirica.onStageEnded(({ stage }) => {
   if (stage.get("name") == "Results"){
